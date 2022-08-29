261bca32a27c1e924ba920656a3ba55ed558ef80
MiguelSwanepoel <576798@student.belgiumcampus.ac.za>
2022-08-29 08:27:02 +0200

    Commit 2

--- src/main/java/com/example/java_crud_api_mysql/controller/StudentController.java
+++ src/main/java/com/example/java_crud_api_mysql/controller/StudentController.java
@@ -14,31 +14,26 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    // END POINT TO FETCH ALL EMPLOYEES
     @GetMapping(value = "/allStudents")
     public List<Student> findAllStudents(){
         return studentService.getAllStudents();
     }
 
-    // END POINT TO FETCH SINGLE EMPLOYEE
     @GetMapping(value = "/findStudent/{id}")
     public Optional<Student> findStudentById(@PathVariable(value = "id") Long stdId){
         return studentService.getStudentById(stdId);
     }
 
-    // END POINT TO ADD NEW EMPLOYEE
     @PostMapping(value = "/addStudent")
     public void addNewStudent(@RequestBody com.example.java_crud_api_mysql.models.Student empObj){
         studentService.addNewStudent(empObj);
     }
 
-    // END POINT TO UPDATE AN EMPLOYEE
     @PutMapping(value = "/updateStudent")
     public void updateNewStudent(@RequestBody Student empObj){
         studentService.updateStudentById(empObj);
     }
 
-    // END POINT TO DELETE AN EMPLOYEE
     @DeleteMapping(value = "/deleteStudent")
     public void deleteNewStudent(@RequestBody Student empObj){
         studentService.deleteStudentById(empObj);
